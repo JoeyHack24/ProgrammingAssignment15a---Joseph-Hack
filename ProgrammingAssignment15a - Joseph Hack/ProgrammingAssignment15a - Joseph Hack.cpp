58f413523f7706c1a691ab3ade126693c9ee9d3f
Joseph Hack <joseph.hack@sinclair.edu>
2024-12-08 17:25:24 -0500

    Add project files.

---
+++ ProgrammingAssignment15a - Joseph Hack/ProgrammingAssignment15a - Joseph Hack.cpp
@@ -0,0 +1,25 @@
+// ProgrammingAssignment15a - Joseph Hack.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Joseph Hack
+// CIS1202
+// December 8, 2024
+
+#include <iostream>
+
+
+char character(char start, int offset);
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+char character(char start, int offset) {
+
+    try {
+
+    }
+    catch () {
+        throw invalidRangeException;
+    }
+}
